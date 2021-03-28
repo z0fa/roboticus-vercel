75ef0e60d7e232ae36db299578b68feb888f8e82
Giovanni Cavallo <giovannicavallo95@gmail.com>
2021-03-28 14:13:04 +0200

    add command scope

--- commands/hello.ts
+++ commands/hello.ts
@@ -3,6 +3,7 @@ import { defineCommand } from "@z0fa/roboticus";
 export default defineCommand({
   name: "hello",
   description: "hello",
+  scope: ["global"],
   handler(req, res) {
     return res.message("hello world!!!")
   },
